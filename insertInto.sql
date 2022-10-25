3ab952c3f38231099ac21fbcac8cc75959838a2b
Junior <juniorjeffersontreze@gmail.com>
2022-10-25 10:05:29 -0300

    inserir na tabela

---
+++ insertInto.sql
@@ -0,0 +1,9 @@
+INSERT INTO tbproduto(
+PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA)
+VALUE('1037797','Clean - 2 Litros - Laranja','PET','2 Litros','Laranja',16.1);
+
+INSERT INTO tbvendedores(
+MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUE('00235','Márcio Almeida Silva',0.8);
+
+/*Não existe a real necessidade de descrever os atributos antes da inserção*\
