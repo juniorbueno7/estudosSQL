faea9e1ee346ec020c2c8c4049413f341f4ff281
Junior <juniorjeffersontreze@gmail.com>
2022-10-25 10:06:35 -0300

    atualizando tabela

---
+++ update.sql
@@ -0,0 +1,8 @@
+UPDATE nomeDaTabela SET coluna = novoValor, coluna = novoValor (se tiver mais de uma alteração)
+WHERE coluna = algumIdentificador/ geralmente chave primária.
+
+UPDATE tbvendedores SET PERCENTUAL_COMISSAO = 0.08
+WHERE MATRICULA = '00235';
+
+UPDATE tbvendedores SET NOME = 'José Geraldo da Fonseca Junior'
+WHERE MATRICULA = '00233';
