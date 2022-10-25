40b12268a8a47a47fbd7b1097da5eb5bd6a42ae6
Junior <juniorjeffersontreze@gmail.com>
2022-10-25 10:23:14 -0300

    adicionando primary key e coluna

---
+++ alterTable.sql
@@ -0,0 +1,5 @@
+/*Apontando uma coluna já existente como primary key*/
+ALTER TABLE nomeTabela ADD PRIMARY KEY (nome da coluna que deseja ser a primary key);
+
+/*Acrescentando uma coluna numa tabela já existente*/
+ALTER TABLE nomeTabela ADD COLUMN (nomeDaColuna tipoDaColuna)
