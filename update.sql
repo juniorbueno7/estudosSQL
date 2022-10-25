6740a6ea5d97d0456f90086a4a471467ca3b55de
Junior <juniorjeffersontreze@gmail.com>
2022-10-25 10:58:21 -0300

    atualizando tabelas

--- update.sql
+++ update.sql
@@ -1,13 +1,16 @@
 UPDATE nomeDaTabela SET coluna = novoValor, coluna = novoValor (se tiver mais de uma alteração)
 WHERE coluna = algumIdentificador/ geralmente chave primária.
 
+/*exemplos*/
+UPDATE tbvendedores SET DATA_ADMISSAO = '2014-08-15', DE_FERIAS = 0 WHERE MATRICULA = '00235';
+
+UPDATE tbvendedores SET DATA_ADMISSAO = '2013-09-17', DE_FERIAS = 1 WHERE MATRICULA = '00236';
+
 UPDATE tbvendedores SET PERCENTUAL_COMISSAO = 0.08
 WHERE MATRICULA = '00235';
 
 UPDATE tbvendedores SET NOME = 'José Geraldo da Fonseca Junior'
 WHERE MATRICULA = '00233';
 
-/*exemplos*/
-UPDATE tbvendedores SET DATA_ADMISSAO = '2014-08-15', DE_FERIAS = 0 WHERE MATRICULA = '00235';
-
-UPDATE tbvendedores SET DATA_ADMISSAO = '2013-09-17', DE_FERIAS = 1 WHERE MATRICULA = '00236';
+/*Mudando todo valor de uma coluna*/
+UPDATE tbproduto SET SABOR = 'Citrus' WHERE SABOR = 'Limão';
