c9ebe289dd3eb45da3e4f99bbaf4a7895019f02c
Junior <juniorjeffersontreze@gmail.com>
2022-10-25 10:23:40 -0300

    deletando tabela ou linha

---
+++ delete.sql
@@ -0,0 +1,5 @@
+/*Deletando toda a tabela*/
+DELETE FROM tbproduto;
+
+/*Deletando uma linha especifica da tb vendedores, usando como referencia a coluna matricula*/ 
+DELETE FROM tbvendedores WHERE MATRICULA = '00233'
